77b849c30907e252dbbdc4ab2079ba0e91150054
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 09:40:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-mouse-dev-57197",
+  url: "https://polished-mouse-dev-57197.botics.co",
+  api: "https://polished-mouse-dev-57197.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
